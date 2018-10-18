30b76e56685efbfcab500a7ea495ddcd82c2efbe
lionelnanchen <lionel.nanchen@heig-vd.ch>
2018-10-18 13:47:56 +0200

    Add the name of the user selected in the user list

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,5 +1,9 @@
 
 $(function () {
+  /**
+   *  Check if the user is already in the list.
+   *  @param String userName  the name of the user to check.
+   */
   function checkUserInList(userName) {
     let users = document.getElementsByClassName("list-group-item");
     for (let i = 0; i < users.length; ++i)
@@ -8,24 +12,28 @@ $(function () {
     return true;
   }
 
+  /**
+   *  Action performed after click on the add-user-button button.
+   *  Add the user in the user list.
+   */
   $(".add-user-button").click(function addUserToList() {
-    let userName = "YOLO";
-
+    let userName = this.parentElement.getElementsByClassName("user-frame-name")[0].textContent; //useID
     if (!checkUserInList(userName))
       return;
 
-    let param = document.createElement("a");
-    param.setAttribute("class", "list-group-item");
-    param.setAttribute("href", "#");
-
-
-
+    let a = document.createElement("a");
+    a.setAttribute("class", "list-group-item");
+    a.setAttribute("href", "#");
     let node = document.createTextNode(userName);
-    param.appendChild(node);
-    let element = document.getElementById("users-list").appendChild(param);
-    element.appendChild(param);
+    a.appendChild(node);
+    let element = document.getElementById("users-list").appendChild(a);
+    element.appendChild(a);
   });
 
+  /**
+   *  Action performed after click on a user frame.
+   *  Open model with the user informations.
+   */
   $(".user-frame-image").click(function openUserInfo() {
     $("#user-info-modal").modal();
   });
