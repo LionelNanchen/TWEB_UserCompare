9bbcbdb27f9b02d47edf506b5544237e2174e5e3
lionelnanchen <lionel.nanchen@heig-vd.ch>
2018-10-19 09:58:09 +0200

    Started user infos modal

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,5 +1,14 @@
 
 $(function () {
+  //Constants
+  const USER_INFOS_REPOS_ALL = "Global number of repo (total/ private/ public):";
+  const USER_INFOS_REPOS_CREATED = "Number of repo created:";
+  const USER_INFOS_REPOS_JOINED = "Number of repo joined:";
+  const USER_INFOS_LINES_ADDED = "Number of added lines of code (++):";
+  const USER_INFOS_LINES_SUB = "Number of subtracted lines of code (--):";
+  const USER_INFOS_LINES_RATIO = "Ratio between added and subtracted lines of code:";
+  const USER_INFOS_ARRAY = [USER_INFOS_REPOS_ALL, USER_INFOS_REPOS_CREATED, USER_INFOS_REPOS_JOINED, USER_INFOS_LINES_ADDED, USER_INFOS_LINES_SUB, USER_INFOS_LINES_RATIO]
+
   /**
   *  Check if the user is already in the list.
   *  @param String userNameID  the id of the list-group-item corresponding to the user.
@@ -50,6 +59,7 @@ $(function () {
 
     let spanUserName = document.createElement("span");
     spanUserName.setAttribute("id", userNameID);
+    spanUserName.setAttribute("class", "users-list-name")
     spanUserName.textContent = userName;
 
     let button = document.createElement("button");
@@ -77,7 +87,35 @@ $(function () {
    *  Action performed after click on a user frame or user name in the users list.
    *  Open model with the user informations.
    */
-  $(document).on("click", ".user-frame-image, .user-list", function openUserInfo() {
+  $(document).on("click", ".user-frame-image, .user-list", function () {
+    userInfoModalBody = $("#user-info-modal-body");
+
+    //image
+    let imgDiv = document.createElement("div");
+    imgDiv.setAttribute("class", "text-center mb-4");
+
+    let img = document.createElement("img");
+    img.setAttribute("class", "rounded-circle user-image");
+    img.setAttribute("src", "images/patseb.jpg"); //CHANGE
+    imgDiv.appendChild(img);
+
+    //infos
+    let infosDiv = document.createElement("div");
+    infosDiv.setAttribute("class", "user-infos");
+
+    for (let i = 0; i < USER_INFOS_ARRAY.length; ++i) {
+      let h3 = document.createElement("h3");
+      h3.textContent = USER_INFOS_ARRAY[i];
+      let p = document.createElement("p");
+      p.textContent = "lol";
+      infosDiv.appendChild(h3);
+      infosDiv.appendChild(p);
+    }
+    //add to modal body
+    userInfoModalBody.append(imgDiv);
+    userInfoModalBody.append(infosDiv);
+
+
     $("#user-info-modal").modal();
   });
 
@@ -85,12 +123,8 @@ $(function () {
    * Action performed after click on a cross in the users list.
    * Delete the user from the list.
    */
-  $(document).on("click", ".delete-user", function deleteUserFromList() {
+  $(document).on("click", ".delete-user", function (e) {
+    e.stopPropagation();
     this.parentElement.parentElement.removeChild(this.parentElement);
-
-    // let user = $("#" + this.getAttribute("id").substring(0, name.length - 6))[0];
-    // user.parentElement.parentElement.removeChild(user.parentElement);
-    // this.parentElement.removeChild(this);
-    //don't forget to delete id in index.html
   });
 });
